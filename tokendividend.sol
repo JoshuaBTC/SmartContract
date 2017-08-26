02adbd5542d8433c43bf0f974e799d8803cf6843
JoshuaBTC <31353524+JoshuaBTC@users.noreply.github.com>
2017-08-26 14:56:48 +0300

    Update tokendividend.sol

--- tokendividend.sol
+++ tokendividend.sol
@@ -72,15 +72,15 @@ contract Utils {
 }
 
 
-contract CSToken is owned, Utils {
+contract "name token" is owned, Utils {
 	struct Dividend {uint256 time; uint256 tenThousandth; uint256 countComplete;}
 
 	/* Public variables of the token */
 	string public standard = 'Token 0.1';
 
-	string public name = 'KickCoin';
+	string public name = 'name token';
 
-	string public symbol = 'KC';
+	string public symbol = 'NMK';
 
 	uint8 public decimals = 8;
 
@@ -120,7 +120,7 @@ contract CSToken is owned, Utils {
 	bool public transfersEnabled = true;
 
 	/* Initializes contract with initial supply tokens to the creator of the contract */
-	function CSToken() {
+	function "name token"() {
 		owner = msg.sender;
 		// So that the index starts with 1
 		dividends.push(Dividend(0, 0, 0));
